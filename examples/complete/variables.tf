c252c0b9923bf8fa7106c4a46239238529d073d4
rahul-infra <rahul.sayam@infraspec.dev>
2025-11-04 12:27:19 +0530

    feat: added input variable for postgres version

--- examples/complete/variables.tf
+++ examples/complete/variables.tf
@@ -122,3 +122,13 @@ variable "force_new_deployment" {
   description = "Whether to force new deployment"
   type        = bool
 }
+
+variable "postgres_engine_version" {
+  description = "The version of the Postgres engine"
+  type        = number
+}
+
+variable "postgres_major_engine_version" {
+  description = "The major version of the Postgres engine"
+  type        = number
+}
